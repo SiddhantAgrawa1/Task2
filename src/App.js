238b5a23b8a7eb20ee5b2c60f1c91d31cbc10ee9
SiddhantAgrawa1 <siddhantagrawal2001@gmail.com>
2022-04-23 02:18:33 +0530

    "Initial Commit"

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React,{Component} from 'react';
+import Card from './Card';
+
+class App extends Component{
+  constructor(){
+    super();
+    this.state = { 
+      user : [],
+      flag : '0'
+    }
+  }
+  componentDidMount(){
+    fetch('https://reqres.in/api/users?page=1')
+    .then(response => response.json())
+    .then(users => this.setState({user : users.data, flag : '1' }));
+  }
+  render(){
+    const {user,flag} = this.state;
+    return(
+      (flag === '1') ? user.map((elem) =>
+        <Card key={elem.id} avatar={elem.avatar} first_name={elem.first_name} last_name={elem.last_name} email={elem.email}/>
+      ) : null
+    );
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(
+  <div>
+    <h1 id='heading'>Our Partners</h1>
+    <div id='cards'> <App /> </div>
+  </div>,
+  document.getElementById('root')
+);
